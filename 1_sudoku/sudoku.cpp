3d48b4010f8f70beb34a79cca620b7818d4d14e1
thuptenDukpa <thuptendukpa10@gmail.com>
2023-07-12 01:47:23 +0530

    Make first check as a function

--- 1_sudoku/sudoku.cpp
+++ 1_sudoku/sudoku.cpp
@@ -7,8 +7,26 @@
 
 using namespace std;
 
-typedef chrono::milliseconds MSEC;
+typedef chrono::microseconds MSEC;
 typedef chrono::high_resolution_clock HRC;
+
+bool check_uniqueness_in_rows(int k, int n, vector<vector<int>>& matrix) {
+    for (int row = 0; row < n; row++) {
+        unordered_set<int> set;
+        //#pragma omp parallel for num_threads(k)
+        for (int col = 0; col < n; col++) {
+            // Check range of numbers
+            if (matrix[row][col] < 1 || matrix[row][col] > n)
+                return false;    
+        
+            set.insert(matrix[row][col]);
+        } 
+        cout << endl;
+        if (set.size() != (long unsigned int)n)
+            return false;
+    }
+    return true;
+}
 
 int main() {
     // Start the timer
@@ -18,7 +36,7 @@ int main() {
     ifstream inputFile("../input.txt");
     int k, n;
     inputFile >> k >> n;
-    cout << "k = " << k << " n = " << n << endl;
+    cout << "k (number of threads) = " << k << " n (dimension of sudoku) = " << n << endl;
 
     vector<vector<int>> matrix(n, vector<int>(n));  
     for (int i = 0; i < n; i++) {
@@ -32,31 +50,24 @@ int main() {
     bool is_valid = true;
 
     // Check: Each row contains unique values from 1 - N.
-    for (int row = 0; row < n; row++) {
-        unordered_set<int> set;
-        for (int col = 0; col < n; col++) {
-            // Check range of numbers
-            if (matrix[row][col] < 1 || matrix[row][col] > n) {
-                is_valid = false;
-                cout << "NO" << endl;
-                break;      
-            }
-            set.insert(matrix[row][col]);
-        } 
-        cout << endl;
-        if (set.size() != (long unsigned int)n)
-            is_valid = false;
-        
-        // cout << "YES" << endl;
-    }
-//      • Each column contains unique values from 1 - N.
-//      • Each of the nXn sub-grids, contains a unique value from 1 - N where, N = n^2
+    is_valid = check_uniqueness_in_rows(k, n, matrix);
+    
+    // Check: Each column contains unique values from 1 - N.
+
+    // Check: Each of the nXn sub-grids, contains a unique value from 1 - N where, N = n^2
 
 
     // Get the execution time in milliseconds
     auto end = HRC::now();
     auto exec_time = chrono::duration_cast<MSEC>(end - start).count();
     cout << exec_time << endl;
+
+    // Generate the output
+    if (is_valid)
+        cout << "Sudoku is valid" << endl;
+    else
+        cout << "Sudoku is invalid" << endl;
     
     return 0;
 }
+
